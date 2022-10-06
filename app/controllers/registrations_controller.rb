c074c61de33ccd14285f64ea14e2b61ba3c9e884
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-06 13:50:54 +0800

    One-to-Many: Forms practice

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,32 @@
 class RegistrationsController < ApplicationController
 
+    before_action :set_event
+
     def index
-        @event = Event.find(params[:event_id])
         @registrations = @event.registrations
     end
+
+    def new
+        @registration = @event.registrations.new
+    end
+
+    def create
+        @registration = @event.registrations.new(registrations_params)
+
+        if @event.save
+            redirect_to event_registrations_url(@event), notice: "Thanks for registering!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+        def registrations_params
+            params.require(:registration).permit(:name, :email, :how_heard)
+        end
+
+        def set_event
+            @event = Event.find(params[:event_id])
+        end
 end
