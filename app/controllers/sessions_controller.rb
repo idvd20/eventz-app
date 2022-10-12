6cb87aed4f9bbfb178d93b6a5f11e404f4793ffa
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-12 09:43:16 +0800

    Sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,8 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        
+        session[:user_id] = nil
+        redirect_to events_url, status: :see_other,
+        notice: "You're now signed out!"
     end
 end
